6adca238e08d078abf9822b3a6662b74d252cef7
Pavlos Ts <tsikrikaspavlos@gmail.com>
2022-12-17 21:56:01 +0200

    Update TextField.cpp

--- MovieBrowser/TextField.cpp
+++ MovieBrowser/TextField.cpp
@@ -1,13 +1,62 @@
-#include "TextField.h"
+#ifndef TEXTFIELD_H
+#define TEXTFIELD_H
 
-void TextField::draw()
-{
-}
+#include "Widget.h"
 
-void TextField::update()
+/*struct TextBox
 {
-}
+private:
 
-void TextField::takeAction(const std::vector<Movie*>& movie_list)
+	float m_positionX{};
+	float m_positionY{};
+
+	bool m_active{ false };
+	bool m_pressed{ false };
+
+public:
+
+	float getPosX() const { return m_positionX; }
+	float setPosX(float x) { m_positionX = x; }
+
+	float getPosY() const { return m_positionY; }
+	float setPosY(float y) { m_positionY = y; }
+
+	bool getActive() const { return m_active; }
+	bool setActive(bool active) { m_active = m_active; }
+
+}; */
+
+class TextField final : public Widget
 {
-}
+private:
+
+	bool m_active{ false };
+	bool m_pressed{ false };
+	const std::string m_text;
+	float m_height{ 0.0f };
+
+public:
+
+	void draw() override;
+	void update() override;
+
+	void takeAction(const std::vector<Movie*>& movie_list) override;
+
+	bool contains(float x, float y) const;
+
+	float getPosX() const { return m_positionX; }
+	float setPosX(float x) { m_positionX = x; }
+
+	float getPosY() const { return m_positionY; }
+	float setPosY(float y) { m_positionY = y; }
+
+	bool getActive() const { return m_active; }
+	bool setActive(bool active) { m_active = m_active; }
+
+	void deleteText();
+
+	TextField(float posX, float posY, const std::string_view text);
+};
+
+
+#endif

dcfe4b76e874a1408a694df26506e2da775099d6
Pavlos Ts <tsikrikaspavlos@gmail.com>
2022-12-18 22:52:31 +0200

    Update TextField.cpp

--- MovieBrowser/TextField.cpp
+++ MovieBrowser/TextField.cpp
@@ -1,6 +1,4 @@
 #include "TextField.h"
-#include <algorithm>
-#include <cctype>
 
 void TextField::draw()
 {
@@ -39,7 +37,6 @@ void TextField::update()
 	mouse_X = graphics::windowToCanvasX(ms.cur_pos_x);
 	mouse_Y = graphics::windowToCanvasY(ms.cur_pos_y);
 
-
 	if (graphics::getKeyState(graphics::SCANCODE_A))
 	{
 		if (!m_typed)
@@ -48,9 +45,9 @@ void TextField::update()
 			characters.push_back('a');
 			iterator++;
 			m_typed = true;
-			m_action = true;
 		}
 	}
+
 	if (graphics::getKeyState(graphics::SCANCODE_B))
 	{
 		if (!m_typed)
@@ -59,7 +56,6 @@ void TextField::update()
 			characters.push_back('b');
 			iterator++;
 			m_typed = true;
-			m_action = true;
 		}
 	}
 
@@ -71,28 +67,281 @@ void TextField::update()
 			characters.push_back('c');
 			iterator++;
 			m_typed = true;
-			m_action = true;
 		}
 	}
+
 	if (graphics::getKeyState(graphics::SCANCODE_D))
 	{
 		if (!m_typed)
 		{
 			m_counter = 0;
-			characters.push_back('c');
+			characters.push_back('d');
 			iterator++;
 			m_typed = true;
-			m_action = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_E))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('e');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_F))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('f');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_G))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('g');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_H))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('h');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_I))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('i');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_J))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('j');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_K))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('k');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_L))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('l');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_M))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('m');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_N))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('n');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_O))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('o');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_P))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('p');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_Q))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('q');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_R))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('r');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_S))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('s');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_T))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('t');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_U))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('u');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_V))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('v');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_W))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('w');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_X))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('x');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_Y))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('y');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_Z))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back('z');
+			iterator++;
+			m_typed = true;
+		}
+	}
+
+	if (graphics::getKeyState(graphics::SCANCODE_SPACE))
+	{
+		if (!m_typed)
+		{
+			m_counter = 0;
+			characters.push_back(' ');
+			iterator++;
+			m_typed = true;
 		}
 	}
 
 	if (graphics::getKeyState(graphics::SCANCODE_BACKSPACE))
 	{
-		if (!characters.empty())
+		if (!characters.empty() && !m_typed)
 		{
 			m_counter = 0;
 			characters.pop_back();
 			iterator--;
+			m_typed = true;
 		}
 	}
 
@@ -109,7 +358,17 @@ void TextField::update()
 
 void TextField::takeAction(const std::vector<Movie*>& movie_list)
 {
-
+	std::string string(characters.begin(), characters.end());
+	for (const auto& movie : movie_list)
+	{
+		if (graphics::getKeyState(graphics::SCANCODE_BACKSPACE))
+		{
+			movie->setDisabled(false);
+		}
+		if (movie->getName().find(string) == std::string::npos)
+			movie->setDisabled(true);
+		
+	}
 }
 
 bool TextField::contains(float x, float y) const
@@ -117,11 +376,10 @@ bool TextField::contains(float x, float y) const
 	return FunctionsConst::distance(x, y, m_positionX, m_positionY);
 }
 
-void TextField::deleteText() {
-
+void TextField::deleteText() 
+{
 	characters.clear();
 }
 
 TextField::TextField(float posX, float posY, const std::string_view text)
-	:Widget(posX, posY), m_text{ text }
-{}
+	:Widget(posX, posY), m_text{ text } {}
